316d8ffd80cb1093678aa283786fc352e4c70c26
hamuhamu <hrdng4922000@yahoo.co.jp>
2017-10-25 11:19:01 +0900

    テストコードtestMultiplicationのリファクタリング

--- test/MoneyTest.php
+++ test/MoneyTest.php
@@ -13,12 +13,9 @@ class MoneyTest extends TestCase
     public function testMultiplication()
     {
         $five = new Dollar(5);
-        $product = $five->times(2);
 
-        $this->assertSame(10, $product->amount);
-
-        $product = $five->times(3);
-        $this->assertSame(15, $product->amount);
+        $this->assertEquals(new Dollar(10), $five->times(2));
+        $this->assertEquals(new Dollar(15), $five->times(3));
     }
 
     /**
