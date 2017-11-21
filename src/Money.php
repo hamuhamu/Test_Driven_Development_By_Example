34dcdf496da13bd29a4c33c8a83f4505a7fa956a
hamuhamu <hrdng4922000@yahoo.co.jp>
2017-11-21 23:07:12 +0900

    Moneyのファクトリメソッドで子クラスではなくMoneyを返すように変更した

--- src/Money.php
+++ src/Money.php
@@ -33,14 +33,14 @@ class Money
         return $this->amount . ' ' . $this->currency;
     }
 
-    public static function dollar(int $amount): Dollar
+    public static function dollar(int $amount): Money
     {
-        return new Dollar($amount, 'USD');
+        return new Money($amount, 'USD');
     }
 
-    public static function franc(int $amount): Franc
+    public static function franc(int $amount): Money
     {
-        return new Franc($amount, 'CHF');
+        return new Money($amount, 'CHF');
     }
 
     public function currency(): String
