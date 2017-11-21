6d6f448e6026a1ac9e19117b9ad11d67568d210d
hamuhamu <hrdng4922000@yahoo.co.jp>
2017-11-21 23:00:27 +0900

    FrancとDollarのtimesメソッドでMoneyを返すようにした

--- src/Dollar.php
+++ src/Dollar.php
@@ -11,6 +11,6 @@ class Dollar extends Money
 
     public function times(int $multiplier): Money
     {
-        return new Dollar($this->amount * $multiplier, $this->currency);
+        return new Money($this->amount * $multiplier, $this->currency);
     }
 }
