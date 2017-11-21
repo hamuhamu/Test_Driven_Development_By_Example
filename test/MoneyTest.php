16a1e261e7a58390f9e3af64147b499f1d1c7e0c
hamuhamu <hrdng4922000@yahoo.co.jp>
2017-11-21 23:12:06 +0900

    不要になったテストを削除した

--- test/MoneyTest.php
+++ test/MoneyTest.php
@@ -24,8 +24,6 @@ class MoneyTest extends TestCase
     {
         $this->assertTrue(Money::dollar(5)->equals(Money::dollar(5)));
         $this->assertFalse(Money::dollar(5)->equals(Money::dollar(6)));
-        $this->assertTrue(Money::franc(5)->equals(Money::franc(5)));
-        $this->assertFalse(Money::franc(5)->equals(Money::franc(6)));
         $this->assertFalse(Money::franc(5)->equals(Money::dollar(5)));
     }
 
@@ -47,13 +45,5 @@ class MoneyTest extends TestCase
     {
         $this->assertEquals('USD', Money::dollar(1)->currency());
         $this->assertEquals('CHF', Money::franc(1)->currency());
-    }
-
-    /**
-     * @test
-     */
-    public function testDifferentClassEquality()
-    {
-        $this->assertTrue((new Money(10, 'CHF'))->equals(new Franc(10, 'CHF')));
     }
 }
